f3640700adedcb410ca72966253675e3a32dd49e
beckrwilliams <beckrwilliams@gmail.com>
2024-10-08 21:53:57 -0700

    Set up the group project. Created 3 projects, RoomateFinder, RoomateFinderEngine, and RoomateFinderTests. The RoomateFinder is set up as the main entry to the program and as the UI layer. The engine is a library project that will contain all of the internal logic for the app, and the Testing project will be used to test our project.

---
+++ RoomateFinder/RoomateFinderTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RoomateFinderTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
