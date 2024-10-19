bcb9ab22b13967eac3cc8a6caf21af11dc00addf
beckrwilliams <beckrwilliams@gmail.com>
2024-10-19 12:58:14 -0700

    renamed ProfileLoader to ProfileManager for better clarity of it's function.

--- RoomateFinder/RoomateFinderEngne/ProfileManager.cs
+++ RoomateFinder/RoomateFinderEngne/ProfileManager.cs
@@ -6,7 +6,7 @@ namespace RoomateFinderEngne
     /// <summary>
     /// reads from a file and populates a dictionary with user profiles. These profiles can then be accessed, worked on, removed, added to etc.
     /// </summary>
-    internal class ProfileLoader
+    internal class ProfileManager
     {
         /// <summary>
         /// stores the users username as a key and its profile as its value.
