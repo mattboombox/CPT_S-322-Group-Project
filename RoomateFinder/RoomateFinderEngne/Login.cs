f96238b83d25e9c05860f371d7f9231e000843de
beckrwilliams <beckrwilliams@gmail.com>
2024-10-20 21:59:37 -0700

    Added login functionality. I still have a couple of features to complete but the basic functionality is currently working.

--- RoomateFinder/RoomateFinderEngne/Login.cs
+++ RoomateFinder/RoomateFinderEngne/Login.cs
@@ -3,27 +3,40 @@
 
 namespace RoomateFinderEngne
 {
-    internal class Login
+    /// <summary>
+    /// Contains methods and properties used in the login process.
+    /// </summary>
+    public class Login
     {
-        
         private string username = "emptyName";
-
         private string password = "emptyPassword";
-
         public string Username { get { return username; } set { username = value; } }
         public string Password { get { return password; } set { password = value; } }
 
+        /// <summary>
+        /// creates a new user.
+        /// </summary>
         public void CreateNewUser()
         {
 
         }
 
+        /// <summary>
+        /// validates the user info.
+        /// </summary>
+        /// <returns>true if the user info is correct.</returns>
         public bool ValidateUserInfo()
         {
-            bool validated = false;
+            bool success = false;
+            PasswordManager pV = new PasswordManager();
 
+            if (pV.Validate(username, password)) 
+            {
+                success = true;
+            }
 
-            return validated;
+            return success;
+            
         }
     }
 }
--- RoomateFinder/RoomateFinderEngne/ProfileManager.cs
+++ RoomateFinder/RoomateFinderEngne/ProfileManager.cs
@@ -11,18 +11,61 @@ namespace RoomateFinderEngne
         /// <summary>
         /// stores the users username as a key and its profile as its value.
         /// </summary>
-        private Dictionary<string, UserProfile> UserProfiles = new Dictionary<string, UserProfile>();
+        private Dictionary<string, UserProfile> userProfiles = new Dictionary<string, UserProfile>();
+        private string path = "profiles.csv";
+        StreamReader reader = null;
 
-        public void CreateNewProfile(string username, string password)
+        /// <summary>
+        /// Constructor for the profile manager
+        /// </summary>
+        /// <exception cref="FileNotFoundException">if the csv file is missing an exception is thrown.</exception>
+        public ProfileManager() 
         {
-
+            if (File.Exists(path))
+            {
+                reader = new StreamReader(File.OpenRead(path));
+            }
+            else
+            {
+                throw new FileNotFoundException("profiles.csv not found.");
+            }
+            LoadProfileDictionary();
         }
 
+        /// <summary>
+        /// Adds a new user profile to the dictionary with the username as the key.
+        /// </summary>
+        /// <param name="username"></param>
+        public void CreateNewProfile(string username)
+        {
+            UserProfile profile = new UserProfile();
+            profile.Username = username;
+            userProfiles.Add(username, profile);
+        }
+
+        /// <summary>
+        /// returns the userprofile with the given username
+        /// </summary>
+        /// <param name="username">the user name of the profile to be loaded.</param>
+        /// <returns>the user profile.</returns>
         public UserProfile LoadProfile(string username) 
         {
-            return UserProfiles[username];
+            return userProfiles[username];
         }
 
-
+        /// <summary>
+        /// reads the csv file and loads all the data into the user profile. ATTENTION: This code will have to be changed when new properties are added to the user profile!
+        /// </summary>
+        private void LoadProfileDictionary()
+        {
+            while (!reader.EndOfStream)
+            {
+                UserProfile profile = new UserProfile();
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                profile.Username = values[0];
+                userProfiles.Add(profile.Username, profile);
+            }
+        }
     }
 }
