3888170ec7efb213e94a30b0f13d53c1c571454b
jnelson <jnelson@eecs.wsu.edu>
2024-10-24 15:10:07 -0700

    Added the ability to edit bio in the profile section of the home page, saves to class attribute, not csv yet working on that now

--- RoomateFinder/RoomateFinder/ProfilePage.Designer.cs
+++ RoomateFinder/RoomateFinder/ProfilePage.Designer.cs
@@ -4,11 +4,18 @@ namespace RoomateFinder
     partial class ProfilePage{
         private Label name;
         private Label bio;
+        private TextBox bioEditor;
 
         private void InitializeComponent()
         {
             name = new Label();
             bio = new Label();
+            bioEditor = new TextBox();
+            menuStrip1 = new MenuStrip();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            usernameToolStripMenuItem = new ToolStripMenuItem();
+            bioToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // name
@@ -20,19 +27,71 @@ namespace RoomateFinder
             // 
             // bio
             // 
-            bio.Location = new Point(200, 50);
+            bio.Location = new Point(50, 200);
             bio.Name = "bio";
-            bio.Size = new Size(50, 30);
+            bio.Size = new Size(300, 400);
             bio.TabIndex = 0;
+
+            // 
+            // bioEditor
+            // 
+            bioEditor.Location = new Point(50, 200);
+            bioEditor.Name = "bioEditor";
+            bioEditor.Size = new Size(300, 400);
+            bioEditor.Visible = false; // Initially hidden
+            bioEditor.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { editToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usernameToolStripMenuItem, bioToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // usernameToolStripMenuItem
+            // 
+            usernameToolStripMenuItem.Name = "usernameToolStripMenuItem";
+            usernameToolStripMenuItem.Size = new Size(224, 26);
+            usernameToolStripMenuItem.Text = "Username";
+            // 
+            // bioToolStripMenuItem
+            // 
+            bioToolStripMenuItem.Name = "bioToolStripMenuItem";
+            bioToolStripMenuItem.Size = new Size(224, 26);
+            bioToolStripMenuItem.Text = "Bio";
+            bioToolStripMenuItem.Click += bioToolStripMenuItem_Click;
             // 
             // ProfilePage
             // 
             ClientSize = new Size(800, 450);
             Controls.Add(name);
             Controls.Add(bio);
+            Controls.Add(bioEditor);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "ProfilePage";
             Text = "Profile Page";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
+
+            this.ActiveControl = name;
         }
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem usernameToolStripMenuItem;
+        private ToolStripMenuItem bioToolStripMenuItem;
     }
 }
--- RoomateFinder/RoomateFinder/ProfilePage.cs
+++ RoomateFinder/RoomateFinder/ProfilePage.cs
@@ -27,5 +27,32 @@ namespace RoomateFinder
             // Add other user details as needed
         }
 
+        private void bioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bio.Visible = false; // Hide the label
+            bioEditor.Text = bio.Text;
+            bioEditor.Visible = true; // Show the TextBox
+            bioEditor.Focus(); // Set focus to the TextBox to start editing
+
+            bioEditor.KeyDown += new KeyEventHandler(BioEditor_Enter); // Add event handler to save on Enter
+        }
+
+        private void BioEditor_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {              
+                bio.Text = bioEditor.Text; // Update the label with the edited text
+                bio.Visible = true; // Show the label again
+                bioEditor.Visible = false; // Hide the TextBox
+
+                var user = controller.GetActiveUser();
+                user.Bio = bioEditor.Text; // Update the bio in the user profile
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                bio.KeyDown -= new KeyEventHandler(BioEditor_Enter);
+            }
+        }
     }
 }
