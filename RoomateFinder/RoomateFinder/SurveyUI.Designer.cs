dcb8c24befe41099b38c96e5de8514a00af583a5
jnelson <jnelson@eecs.wsu.edu>
2024-12-04 11:57:25 -0800

    All questions are hard coded into the Survey UI now

--- RoomateFinder/RoomateFinder/SurveyUI.Designer.cs
+++ RoomateFinder/RoomateFinder/SurveyUI.Designer.cs
@@ -75,7 +75,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(11, 68);
+            label1.Location = new Point(12, 68);
             label1.Name = "label1";
             label1.Size = new Size(397, 40);
             label1.TabIndex = 0;
@@ -83,14 +83,14 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(11, 111);
+            textBox1.Location = new Point(12, 111);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(200, 27);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(11, 191);
+            textBox2.Location = new Point(12, 191);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(200, 27);
             textBox2.TabIndex = 3;
@@ -98,7 +98,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(11, 148);
+            label2.Location = new Point(12, 148);
             label2.Name = "label2";
             label2.Size = new Size(358, 40);
             label2.TabIndex = 2;
@@ -106,7 +106,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(11, 271);
+            textBox3.Location = new Point(12, 271);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(200, 27);
             textBox3.TabIndex = 5;
@@ -114,7 +114,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(11, 228);
+            label3.Location = new Point(12, 228);
             label3.Name = "label3";
             label3.Size = new Size(348, 40);
             label3.TabIndex = 4;
@@ -122,7 +122,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(11, 351);
+            textBox4.Location = new Point(12, 351);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(200, 27);
             textBox4.TabIndex = 7;
@@ -130,7 +130,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(11, 308);
+            label4.Location = new Point(12, 308);
             label4.Name = "label4";
             label4.Size = new Size(371, 40);
             label4.TabIndex = 6;
@@ -138,7 +138,7 @@
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(11, 431);
+            textBox5.Location = new Point(6, 437);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(200, 27);
             textBox5.TabIndex = 9;
@@ -146,7 +146,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(11, 388);
+            label5.Location = new Point(6, 394);
             label5.Name = "label5";
             label5.Size = new Size(440, 40);
             label5.TabIndex = 8;
@@ -154,7 +154,7 @@
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(11, 531);
+            textBox6.Location = new Point(6, 544);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(200, 27);
             textBox6.TabIndex = 11;
@@ -162,7 +162,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(11, 468);
+            label6.Location = new Point(6, 481);
             label6.Name = "label6";
             label6.Size = new Size(388, 60);
             label6.TabIndex = 10;
@@ -170,7 +170,7 @@
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(8, 591);
+            textBox7.Location = new Point(6, 610);
             textBox7.Name = "textBox7";
             textBox7.Size = new Size(200, 27);
             textBox7.TabIndex = 13;
@@ -178,7 +178,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(8, 571);
+            label7.Location = new Point(6, 590);
             label7.Name = "label7";
             label7.Size = new Size(391, 20);
             label7.TabIndex = 12;
@@ -186,7 +186,7 @@
             // 
             // textBox8
             // 
-            textBox8.Location = new Point(8, 658);
+            textBox8.Location = new Point(6, 681);
             textBox8.Name = "textBox8";
             textBox8.Size = new Size(200, 27);
             textBox8.TabIndex = 15;
@@ -194,7 +194,7 @@
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(8, 638);
+            label8.Location = new Point(6, 661);
             label8.Name = "label8";
             label8.Size = new Size(424, 20);
             label8.TabIndex = 14;
@@ -202,7 +202,7 @@
             // 
             // textBox9
             // 
-            textBox9.Location = new Point(11, 758);
+            textBox9.Location = new Point(6, 793);
             textBox9.Name = "textBox9";
             textBox9.Size = new Size(200, 27);
             textBox9.TabIndex = 17;
@@ -210,7 +210,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(11, 695);
+            label9.Location = new Point(6, 730);
             label9.Name = "label9";
             label9.Size = new Size(324, 60);
             label9.TabIndex = 16;
@@ -218,7 +218,7 @@
             // 
             // textBox10
             // 
-            textBox10.Location = new Point(8, 838);
+            textBox10.Location = new Point(6, 884);
             textBox10.Name = "textBox10";
             textBox10.Size = new Size(200, 27);
             textBox10.TabIndex = 19;
@@ -226,7 +226,7 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(8, 795);
+            label10.Location = new Point(6, 841);
             label10.Name = "label10";
             label10.Size = new Size(427, 40);
             label10.TabIndex = 18;
@@ -244,7 +244,7 @@
             // 
             // textBox11
             // 
-            textBox11.Location = new Point(457, 825);
+            textBox11.Location = new Point(457, 924);
             textBox11.Name = "textBox11";
             textBox11.Size = new Size(200, 27);
             textBox11.TabIndex = 40;
@@ -252,15 +252,15 @@
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(457, 805);
+            label11.Location = new Point(457, 841);
             label11.Name = "label11";
-            label11.Size = new Size(154, 20);
+            label11.Size = new Size(369, 80);
             label11.TabIndex = 39;
-            label11.Text = "Template Question 10";
+            label11.Text = "\"Are you ok with sharing a space with a roommate's \r\nsignificant other? Yes no issues, \r\nYes but they cannot spend the night,\r\n No but they can visit, No relationships allowed inside\"\r\n";
             // 
             // textBox12
             // 
-            textBox12.Location = new Point(457, 745);
+            textBox12.Location = new Point(457, 804);
             textBox12.Name = "textBox12";
             textBox12.Size = new Size(200, 27);
             textBox12.TabIndex = 38;
@@ -268,15 +268,15 @@
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(457, 725);
+            label12.Location = new Point(457, 741);
             label12.Name = "label12";
-            label12.Size = new Size(146, 20);
+            label12.Size = new Size(399, 60);
             label12.TabIndex = 37;
-            label12.Text = "Template Question 9";
+            label12.Text = "\"How do you feel about hosting events or parties\r\n in the shared space? 24/7 party, Ok with occasional events,\r\n Prefer small gatherings, No parties ever\"\r\n";
             // 
             // textBox13
             // 
-            textBox13.Location = new Point(457, 665);
+            textBox13.Location = new Point(457, 704);
             textBox13.Name = "textBox13";
             textBox13.Size = new Size(200, 27);
             textBox13.TabIndex = 36;
@@ -284,15 +284,15 @@
             // label13
             // 
             label13.AutoSize = true;
-            label13.Location = new Point(457, 645);
+            label13.Location = new Point(457, 661);
             label13.Name = "label13";
-            label13.Size = new Size(146, 20);
+            label13.Size = new Size(295, 40);
             label13.TabIndex = 35;
-            label13.Text = "Template Question 8";
+            label13.Text = "\"Whats your prefered indoor temperature? \r\nHot, warm, Room temperature, Freezing\"\r\n";
             // 
             // textBox14
             // 
-            textBox14.Location = new Point(457, 585);
+            textBox14.Location = new Point(457, 624);
             textBox14.Name = "textBox14";
             textBox14.Size = new Size(200, 27);
             textBox14.TabIndex = 34;
@@ -300,15 +300,15 @@
             // label14
             // 
             label14.AutoSize = true;
-            label14.Location = new Point(457, 565);
+            label14.Location = new Point(457, 581);
             label14.Name = "label14";
-            label14.Size = new Size(146, 20);
+            label14.Size = new Size(370, 40);
             label14.TabIndex = 33;
-            label14.Text = "Template Question 7";
+            label14.Text = "\"Are you okay with splitting costs for shared items\r\n or services? Never, only for cheap stuff, Often, Always\"\r\n";
             // 
             // textBox15
             // 
-            textBox15.Location = new Point(457, 505);
+            textBox15.Location = new Point(457, 544);
             textBox15.Name = "textBox15";
             textBox15.Size = new Size(200, 27);
             textBox15.TabIndex = 32;
@@ -316,15 +316,15 @@
             // label15
             // 
             label15.AutoSize = true;
-            label15.Location = new Point(457, 485);
+            label15.Location = new Point(457, 501);
             label15.Name = "label15";
-            label15.Size = new Size(146, 20);
+            label15.Size = new Size(334, 40);
             label15.TabIndex = 31;
-            label15.Text = "Template Question 6";
+            label15.Text = "\"Are you punctual with rent and utility payments?\r\n Always, Sometimes, Rarley, Never\"\r\n";
             // 
             // textBox16
             // 
-            textBox16.Location = new Point(457, 425);
+            textBox16.Location = new Point(457, 464);
             textBox16.Name = "textBox16";
             textBox16.Size = new Size(200, 27);
             textBox16.TabIndex = 30;
@@ -332,15 +332,15 @@
             // label16
             // 
             label16.AutoSize = true;
-            label16.Location = new Point(457, 405);
+            label16.Location = new Point(457, 421);
             label16.Name = "label16";
-            label16.Size = new Size(146, 20);
+            label16.Size = new Size(404, 40);
             label16.TabIndex = 29;
-            label16.Text = "Template Question 5";
+            label16.Text = "\"Would you prefer to be friends with your roommate or\r\n keep it professional? Best friends, friends, neutral, enemies\"";
             // 
             // textBox17
             // 
-            textBox17.Location = new Point(457, 345);
+            textBox17.Location = new Point(457, 391);
             textBox17.Name = "textBox17";
             textBox17.Size = new Size(200, 27);
             textBox17.TabIndex = 28;
@@ -348,15 +348,15 @@
             // label17
             // 
             label17.AutoSize = true;
-            label17.Location = new Point(457, 325);
+            label17.Location = new Point(457, 328);
             label17.Name = "label17";
-            label17.Size = new Size(146, 20);
+            label17.Size = new Size(404, 60);
             label17.TabIndex = 27;
-            label17.Text = "Template Question 4";
+            label17.Text = "\"How do you feel about decorating shared spaces? \r\nwould enjoy decorating together, Okay with it occasionally, \r\nPrefer no changes, Only I can decorate\"\r\n";
             // 
             // textBox18
             // 
-            textBox18.Location = new Point(457, 265);
+            textBox18.Location = new Point(457, 294);
             textBox18.Name = "textBox18";
             textBox18.Size = new Size(200, 27);
             textBox18.TabIndex = 26;
@@ -364,15 +364,15 @@
             // label18
             // 
             label18.AutoSize = true;
-            label18.Location = new Point(457, 245);
+            label18.Location = new Point(457, 251);
             label18.Name = "label18";
-            label18.Size = new Size(146, 20);
+            label18.Size = new Size(398, 40);
             label18.TabIndex = 25;
-            label18.Text = "Template Question 3";
+            label18.Text = "\"Are you comfortable sharing items? No sharing, \r\nminimal sharing, open to sharing, abolish private property\"";
             // 
             // textBox19
             // 
-            textBox19.Location = new Point(457, 185);
+            textBox19.Location = new Point(457, 208);
             textBox19.Name = "textBox19";
             textBox19.Size = new Size(200, 27);
             textBox19.TabIndex = 24;
@@ -380,15 +380,15 @@
             // label19
             // 
             label19.AutoSize = true;
-            label19.Location = new Point(457, 165);
+            label19.Location = new Point(457, 145);
             label19.Name = "label19";
-            label19.Size = new Size(146, 20);
+            label19.Size = new Size(400, 60);
             label19.TabIndex = 23;
-            label19.Text = "Template Question 2";
+            label19.Text = "\"How much personal space do you need? \r\nI need a lot of alone time,Moderate balance,\r\n I enjoy spending time with others often, I cannot be alone\"";
             // 
             // textBox20
             // 
-            textBox20.Location = new Point(457, 105);
+            textBox20.Location = new Point(457, 111);
             textBox20.Name = "textBox20";
             textBox20.Size = new Size(200, 27);
             textBox20.TabIndex = 22;
@@ -396,16 +396,16 @@
             // label20
             // 
             label20.AutoSize = true;
-            label20.Location = new Point(457, 85);
+            label20.Location = new Point(457, 48);
             label20.Name = "label20";
-            label20.Size = new Size(146, 20);
+            label20.Size = new Size(279, 60);
             label20.TabIndex = 21;
-            label20.Text = "Template Question 1";
+            label20.Text = "\"How do you prefer to resolve conflicts? \r\nOpen discussion, Mediated conversation,\r\n Avoid confrontation, Physical combat\"";
             // 
             // label21
             // 
             label21.AutoSize = true;
-            label21.Location = new Point(302, 7);
+            label21.Location = new Point(71, 2);
             label21.Name = "label21";
             label21.Size = new Size(259, 40);
             label21.TabIndex = 41;
@@ -416,7 +416,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(882, 903);
+            ClientSize = new Size(882, 973);
             Controls.Add(label21);
             Controls.Add(textBox11);
             Controls.Add(label11);
